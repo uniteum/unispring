ec998d73644987a9b6e3b489b49a578fcd141a0f
Paul Reinholdtsen <paul@bitsy.services>
2026-04-11 13:06:22 -0500

    pragma solidity ^0.8.30;

--- test/Unispring.t.sol
+++ test/Unispring.t.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.30;
+pragma solidity ^0.8.30;
 
 import {Unispring} from "../src/Unispring.sol";
 import {IAddressLookup} from "ilookup/IAddressLookup.sol";
