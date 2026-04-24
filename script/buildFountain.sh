b0468243868c02519b3f983ea62016f0f27df60d
Paul Reinholdtsen <paul@bitsy.services>
2026-04-24 15:08:17 -0500

    Fountain: write input file without trailing newline

--- script/buildFountain.sh
+++ script/buildFountain.sh
@@ -2,4 +2,4 @@ forge script script/FountainDeploy.s.sol -f $chain
 transaction=$(jq -r '.transactions[0]' broadcast/FountainDeploy.s.sol/$chain/dry-run/run-latest.json)
 Fountain=$(echo "$transaction" | jq -r '.contractAddress')
 forge verify-contract $Fountain Fountain --chain $chain --verifier etherscan --show-standard-json-input | jq '.'> io/Fountain.json
-echo "$transaction" | jq -r '.transaction.input' > io/FountainInput.txt
+echo "$transaction" | jq -j '.transaction.input' > io/FountainInput.txt
