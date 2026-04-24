c14d281b15627db85292e2206d359a438c199189
Paul Reinholdtsen <paul@bitsy.services>
2026-04-24 18:57:59 -0500

    Verify Mimicoinage.sh

--- script/Mimicoinage.sh
+++ script/Mimicoinage.sh
@@ -1,3 +1,7 @@
 source .env
 zero=0x0000000000000000000000000000000000000000000000000000000000000000
-printf '%s' "$(cast concat-hex $zero $(forge inspect Mimicoinage bytecode) $(cast abi-encode "constructor(address,address)" $Fountain $ICoinage))" > io/Mimicoinage.txt
+MimicoinageInput=$(printf '%s' "$(cast concat-hex $zero $(forge inspect Mimicoinage bytecode) $(cast abi-encode "constructor(address,address)" $Fountain $ICoinage))")
+echo "$MimicoinageInput" > io/Mimicoinage.txt
+Mimicoinage=$(cast call $ARACHNID $MimicoinageInput -r 11155111)
+echo "Mimicoinage deployed at: $Mimicoinage"
+forge verify-contract $Mimicoinage Mimicoinage --verifier etherscan --show-standard-json-input | jq '.'> io/Mimicoinage.json
