50157769dca2f3f195368580e3c24a6aa000fbf8
Paul Reinholdtsen <paul@bitsy.services>
2026-04-25 13:30:32 -0500

    delete unwanted copy

--- script/Mimicoinage copy.sh
+++
@@ -1,9 +0,0 @@
-source .env
-salt=0x0000000000000000000000000000000000000000000000000000000000000000
-bytecode=$(forge inspect Mimicoinage bytecode)
-constructorArgs=$(cast abi-encode "constructor(address,address)" $Fountain $ICoinage)
-MimicoinageInput=$(cast concat-hex $salt $bytecode $constructorArgs)
-printf '%s' "$MimicoinageInput" > io/Mimicoinage.txt
-Mimicoinage=$(cast call $ARACHNID $MimicoinageInput -r 11155111)
-echo "Mimicoinage deployed at: $Mimicoinage"
-forge verify-contract $Mimicoinage Mimicoinage --verifier etherscan --show-standard-json-input | jq '.'> io/Mimicoinage.json
