4c5102f06ab39ecb3c11101cde289a272ab4e9d2
Paul Reinholdtsen <paul@bitsy.services>
2026-04-30 17:04:21 -0500

    Mimicry=0x1EFb1eFb242489Dcac7B7bc334EEd34ee9aa3bC3

--- script/Mimicry.sh
+++ script/Mimicry.sh
@@ -1,10 +1,12 @@
 source .env
-salt=0x0000000000000000000000000000000000000000000000000000000000000000
+salt=0x000000000000000000000000000000000000000000000000000000001eab845b
 bytecode=$(forge inspect Mimicry bytecode)
 constructorArgs=$(cast abi-encode "constructor(address,address)" $Fountain $ICoinage)
 initcode=$(cast concat-hex $bytecode $constructorArgs)
 input=$(cast concat-hex $salt $initcode)
 printf '%s' "$input" > io/Mimicry.txt
+initcodehash=$(cast keccak $initcode)
+echo "initcodehash=$initcodehash"
 Mimicry=$(cast create2 --deployer $deployer --salt $salt --init-code $initcode)
 echo "Mimicry=$Mimicry"
 forge verify-contract $Mimicry Mimicry --verifier etherscan --show-standard-json-input | jq '.'> io/Mimicry.json
