d4838e11b38f8f389e6413eaea0c16c4c330f158
Paul Reinholdtsen <paul@bitsy.services>
2026-05-03 11:38:33 -0500

    Remove .env dep on Mimicry.sh

--- script/Mimicry.sh
+++ script/Mimicry.sh
@@ -1,4 +1,6 @@
-source .env
+deployer=0x4e59b44847b379578588920cA78FbF26c0B4956C
+Fountain=0xF0F1d225A78c1EdcD0f5a9E31398Ed0BA3Dee071
+ICoinage=0x1EB8901612767C04b3819E8A743ADCe88F9Fe110
 args=$(cast abi-encode "constructor(address,address)" $Fountain $ICoinage)
 contract=Mimicry
 dir=io/$contract
