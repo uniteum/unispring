4af29f12b557335b07c9cc4921a0d9b55be4b3de
Paul Reinholdtsen <paul@bitsy.services>
2026-05-02 19:25:43 -0500

    Fountain=0xf0F09C38397755c028A6E7A17A5B78f80d3dE070

--- script/Fountain.sh
+++ script/Fountain.sh
@@ -1,10 +1,16 @@
 source .env
-salt=0x0000000000000000000000000000000000000000000000000000000000000000
+mkdir -p io/Fountain
+salt=0x0000000000000000000000000000000000000000000000000000000160644836
 bytecode=$(forge inspect Fountain bytecode)
 constructorArgs=$(cast abi-encode "constructor(address)" $PoolManagerLookup)
 initcode=$(cast concat-hex $bytecode $constructorArgs)
-FountainInput=$(cast concat-hex $salt $initcode)
-printf '%s' "$FountainInput" > script/Fountain.txt
-Fountain=$(cast create2 --deployer $deployer --salt $salt --init-code $initcode)
-echo "Fountain deployed at: $Fountain"
-forge verify-contract $Fountain Fountain --verifier etherscan --show-standard-json-input | jq '.'> script/Fountain.json
+initcodehash=$(cast keccak "$initcode")
+echo "initcodehash=$initcodehash"
+input=$(cast concat-hex $salt $initcode)
+printf '%s' "$input" > io/Fountain/$home.txt
+home=$(cast create2 --deployer $deployer --salt $salt --init-code $initcode)
+echo "home=$home"
+
+mkdir -p io/Fountain
+printf '%s' "$input" > "io/Fountain/$home.txt"
+forge verify-contract $home Fountain --verifier etherscan --show-standard-json-input | jq '.'> io/Fountain/$home.json
