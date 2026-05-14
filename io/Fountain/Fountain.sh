7c278cf36d35685ad7a2465d492a3b3f14c77b8f
Paul Reinholdtsen <paul@bitsy.services>
2026-05-13 21:09:19 -0500

    Mine Fountain 0xf0034cf4b0bB07FaB26B3DC28bbEfe3f5f34e090

--- io/Fountain/Fountain.sh
+++ io/Fountain/Fountain.sh
@@ -4,9 +4,9 @@
 set -euo pipefail
 source "$(git rev-parse --show-toplevel)/lib/crucible/script/proto.sh"
 
-PoolManagerLookup=0xB001Ee991d31e76E0edc4073B24B0C2B7202E220
+PoolManagerLookup=0xb001763Ad7828c7a593EfE5e9A37079D1a7Ee300
 owner=0x9891e323517761F525e55817F1b3fa2C52620b78
 mask=0xfff000000000000000000000000000000000ffff
 target=0xf00000000000000000000000000000000000e090
-proto_predict Fountain 0x0000000000000000000000000000000000000000000000000000000019cd130d \
+proto_predict Fountain 0x0000000000000000000000000000000000000000000000000000000005da92ee \
     "constructor(address,address)" "$owner" "$PoolManagerLookup"
