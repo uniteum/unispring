4be962e70ddbb39b1ac2cc5e3c683e70c807519d
Paul Reinholdtsen <paul@bitsy.services>
2026-05-14 19:30:20 -0500

    verify Reflector

--- io/Reflector/verify.sh
+++ io/Reflector/verify.sh
@@ -2,4 +2,4 @@ bash lib/crucible/script/verify.sh $1 0x1Eb8dF6040A67025C2aF9a82aB966CCd7bF1e300
 bash lib/crucible/script/verify.sh $1 0xaDDb841F7f2D4176C480E3734448047ad83FE300 # AddressLookup
 bash lib/crucible/script/verify.sh $1 0x555F0018a30F59634af07589ed17f44DF2BCE300 # StringLookup
 bash lib/crucible/script/verify.sh $1 0xF00c0C30CE13f01c77C1F8d60Fc1146014B4E090 # Fountain
-bash lib/crucible/script/verify.sh $1 0xBDbd73f71A85feccbB20DBB25B21EaD9a457E090 # Reflector
+bash lib/crucible/script/verify.sh $1 0xBDbd6217ADFe1f3AE9fd4eC4D82d62A3a9baE090 # Reflector
